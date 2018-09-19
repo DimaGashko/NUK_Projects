3d252795356ae1b7a1a16b5db235e57bccbd6784
DimaGashko <dgashko@mail.ua>
2018-09-19 19:40:49 +0300

    updateL1Code

--- Java/Semester 3/Labs/L1/L1.java
+++ Java/Semester 3/Labs/L1/L1.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Scanner;
 import static java.lang.Math.*;
 
@@ -12,55 +13,71 @@ public class Main {
         Main app = new Main();
         app.run();
     }
-    
+
+    private double getA() { return a; }
+    private double getB() { return b; }
+    private double getX() { return x; }
+
+    private void setA(double a) { this.a = a; }
+    private void setB(double b) { this.b = b; }
+    private void setX(double x) { this.x = x; }
+
     private void run() {
         inputStartData();
 
-        double s = calcS(a, b, x);
-        double q = calcQ(a, b, x);
+        double s = calcS(getA(), getB(), getX());
+        double q = calcQ(getA(), getB(), getX());
 
         printResult(s, q);
-        printCurrentDate();
+
+        printCurrentDate("dd MMM yyyy", "uk");
+        printCurrentDate("dd MMM yyyy", "ru");
+        printCurrentDate("dd MMM yyyy", "en");
     }
 
     private void inputStartData() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Input a: ");
-        a = scanner.nextDouble();
+        setA(scanner.nextDouble());
 
         System.out.println("Input b: ");
-        b = scanner.nextDouble();
+        setB(scanner.nextDouble());
 
         System.out.println("Input x: ");
-        x = scanner.nextDouble();
+        setX(scanner.nextDouble());
     }
 
     private void printResult(double s, double q) {
         System.out.println("Input data:");
-        System.out.println("a: " + a);
-        System.out.println("b: " + b);
-        System.out.println("x: " + x + "\n");
+        System.out.println("a: " + getA());
+        System.out.println("b: " + getB());
+        System.out.println("x: " + getX() + "\n");
 
         System.out.println("Results:");
         System.out.println("s: " + s);
-        System.out.println("q: " + q);
+        System.out.println("q: " + q + "\n");
     }
 
-    private void printCurrentDate() {
+    private void printCurrentDate(String format, String lan) {
         LocalDate currentDate = LocalDate.now();
 
         System.out.print("Current Date: ");
-        System.out.println(currentDate.format(DateTimeFormatter.ofPattern("dd MM uuuu")));
+
+        System.out.println(currentDate.format(
+                DateTimeFormatter.ofPattern(format, new Locale(lan))
+        ));
     }
 
     private double calcS(double a, double b, double x) {
-        return pow(x, 3) * pow(tan(pow(x + b, 2)), 2)
-                + a / sqrt(x + b);
+        double t = tan((x + b) * (x + b));
+        return (x*x*x) * (t*t) + a / sqrt(x + b);
     }
 
     private double calcQ(double a, double b, double x) {
-        return (b * pow(x, 2) - a) / (exp(a * x) - 1);
+        return (b * x*x - a) / (exp(a * x) - 1);
     }
+
+
 
 }
