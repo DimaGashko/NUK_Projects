6a2dfe972371bed2d3fb84fd2d698129387feecb
DimaGashko <dgashko@mail.ua>
2018-11-27 19:50:29 +0200

    l5Code

---
+++ Дискретные структуры/L5/task2.pl
@@ -0,0 +1,20 @@
+%Jane and Alan - Alex and Josh; Alexa and Kim - Alan; Jady and Jack - Kim
+%       Alex and Josh
+% -Jane              Alan
+%             Alexa         Kim
+%                      Jady       Jack
+
+mom(jane, alex).
+mom(alexa, alan).
+mom(jady, kim).
+mom(jane, josh).
+dad(alan, alex).
+dad(alan, josh).
+dad(kim, alan).
+dad(jack, kim).
+
+% X is Y's brother or sister
+sibling(X, Y) :- mom(Mom, X), mom(Mom, Y), dad(Dad, X), dad(Dad, Y), X \= Y.
+
+% X is Y's dad
+grandfather(X, Y) :- dad(X, D), dad(D, Y).
