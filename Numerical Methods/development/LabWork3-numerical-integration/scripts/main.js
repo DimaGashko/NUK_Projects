727f2b066dc5ace2ae5ff58d88d7299b3354b8a7
DimaGashko <dgashko@mail.ua>
2019-03-29 21:12:14 +0200

    baseValidation

--- Numerical Methods/development/LabWork3-numerical-integration/scripts/main.js
+++ Numerical Methods/development/LabWork3-numerical-integration/scripts/main.js
@@ -1,17 +1,39 @@
 ;(function(){
 
-   var els = {
-      form: document.querySelector(".setup_form"),
-   }
+   const form = document.querySelector(".setup_form");
 
-   initEvents();
+   //initEvents();
 
    function calculate() { 
-      
+      const func = form.func.value;
+      const precision = parseFloat(form.precision.value);
+      const from = parseFloat(form.from.value);
+      const to = parseFloat(form.to.value);
+      const method = form.method.value;
+
+      switch (method) { 
+         case 'simpson':
+            integrateBySimpson(func, from, to, precision);
+            break;
+         case 'trapezoidal':
+            integrateByTrapezoidal(func, from, to, precision);
+            break;
+         case 'middle-rectangular':
+         case 'right-rectangular':
+         case 'left-rectangular':
+            integrateByRectangular(func, from, to, precision, method);
+            break;
+         default:
+            printCalcError();
+      }
+   }
+
+   function printCalcError() {
+
    }
 
    function initEvents() { 
-      els.form.addEventListener('submit', (event) => {
+      form.addEventListener('submit', (event) => {
          event.preventDefault();
 
          calculate();         
