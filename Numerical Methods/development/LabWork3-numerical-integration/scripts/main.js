3cb18ac54a89243b246f0e7f054947732b3d0353
DimaGashko <dgashko@mail.ua>
2019-03-30 16:05:31 +0200

    baseRes

--- Numerical Methods/development/LabWork3-numerical-integration/scripts/main.js
+++ Numerical Methods/development/LabWork3-numerical-integration/scripts/main.js
@@ -1,16 +1,19 @@
 ;(function(){
 
-   const form = document.querySelector(".setup_form");
+   const els = {
+      form: document.querySelector(".setup_form"),
+      results: document.querySelector(".results-text"),
+   }
 
    initEvents();
 
    function calculate() { 
       const func = getFunction();
       window.func = func;
-      const precision = parseFloat(form.precision.value);
-      const from = parseFloat(form.from.value);
-      const to = parseFloat(form.to.value);
-      const method = form.method.value;
+      const precision = parseFloat(els.form.precision.value);
+      const from = parseFloat(els.form.from.value);
+      const to = parseFloat(els.form.to.value);
+      const method = els.form.method.value;
 
       let res;
 
@@ -23,23 +26,38 @@
          throw err;
       }
 
-      console.log(res);
+      printResults(res, precision);
    }
 
-   function getFunction() { 
+   /**
+    * @param {{n: number, val: number}[]} res 
+    */
+   function printResults(res, precision) { 
+      const digits = Math.abs(Math.floor(Math.log10(precision)));
+      console.log(digits);
+      
+      const strRes = res.reverse()
+         .map((item) => `I = ${item.val.toFixed(digits)} (n = ${item.n})`)
+         .join('<br>');
+      
+      els.results.innerHTML = strRes;
+   }
+
+   function getFunction() {
+      let strFunc = els.form.func.value;
+
       return (x) => { 
-         const strFunc = form.func.value.replace(/x/gi, '' + x);
+         strFunc = strFunc.replace(/x/gi, '' + x);
          return parseEquation(strFunc);
       };
    }
 
    function printCalcError() {
       console.log("Error");
-      
    }
 
    function initEvents() { 
-      form.addEventListener('submit', (event) => {
+      els.form.addEventListener('submit', (event) => {
          event.preventDefault();
 
          calculate();         
