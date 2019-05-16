a08bd05e180452dc13dbbf7f5e1f1d980b03c400
DimaGashko <dimagashko@gmail.com>
2019-05-16 07:10:42 +0300

    basePaage

--- Numerical Methods/development/LabWork5-approximate/src/pages/index/index.js
+++ Numerical Methods/development/LabWork5-approximate/src/pages/index/index.js
@@ -1,2 +1,8 @@
+import tmpl from 'lodash/template';
 import './index.sass';
 
+import template from './index.template.html';
+import data from './data.js';
+
+const $contentSlot = document.querySelector('.content-slot');
+$contentSlot.innerHTML = tmpl(template)(data);
