e7b560b19e6db3f33117031ad52b7de8a67f3036
DimaGashko <dimagashko@gmail.com>
2019-05-16 08:21:52 +0300

    baseSolutions

--- Numerical Methods/development/LabWork5-approximate/src/pages/index/index.js
+++ Numerical Methods/development/LabWork5-approximate/src/pages/index/index.js
@@ -7,8 +7,13 @@ import Model from './model.js';
 
 const model = new Model();
 const approximate = model.approximate();
-const approximateYs = approximate.map(item => +item.y.toFixed(2)); 
-const xs = approximate.map(item => +item.x.toFixed(2)); 
+const approximateYs = approximate.map(item => +item.y.toFixed(2));
+const xs = approximate.map(item => +item.x.toFixed(2));
+const inputDataPoints = model.data.xs.map((x, i) => {
+    return {
+        x, y: model.data.ys[i]
+    };
+});
 
 const $contentSlot = document.querySelector('.content-slot');
 
@@ -19,17 +24,41 @@ var ctx = $canvas.getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: xs,
+        labels: xs.map(x => `x: ${x}`),
         datasets: [{
-            label: 'value',
+            label: 'Approximate Function',
             data: approximateYs,
             backgroundColor: 'transparent',
             borderColor: 'rgb(0,110,135)',
+        }, {
+            label: 'Input Data',
+            data: inputDataPoints,
+            backgroundColor: 'transparent',
+            borderColor: 'transparent',
+            pointBackgroundColor: '#6464cd',
+            type: 'scatter',
+        }],
+    },
+});
+
+/**
+
+var myChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Approximate Function',
+            data: approximate,
+            backgroundColor: '#6464cd',//'transparent',
         }],
     },
     options: {
         scales: {
-            
+
         }
     }
 });
+
+
+
+ */
