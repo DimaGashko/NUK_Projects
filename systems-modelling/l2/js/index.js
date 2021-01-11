b98cad02281a02295539936702188965e0d919f6
DimaGashko <dimagashko@gmail.com>
2021-01-11 05:13:46 +0200

    initForm

--- systems-modelling/l2/js/index.js
+++ systems-modelling/l2/js/index.js
@@ -1,3 +1,36 @@
-import { lcg } from "./random.js";
+import { debounce } from "../node_modules/throttle-debounce/esm/index.js";
 
-window.lcg = lcg;
+const p = document.querySelector('.page-js');
+const args = p.querySelector('.args-js');
+
+initEvents();
+
+function initEvents() {
+   const delayedUpdate = debounce(500, update);
+
+   args.addEventListener('submit', (e) => {
+      e.preventDefault();
+      update();
+   });
+
+   args.addEventListener('input', delayedUpdate);
+   args.addEventListener('change', delayedUpdate);
+}
+
+function update() {
+   const args = getArgs();
+   
+}
+
+function getArgs() {
+   return {
+      b1: +args.b1.value,
+      b2: +args.b2.value,
+      sigma: +args.sigma.value,
+      y0: +args.y0.value,
+      c1: +args.c1.value,
+      c3: +args.c3.value,
+      w: +args.w.value,
+      epsilon: +args.epsilon.value,
+   }
+}
